d9d50c537a7306a59dde0ab08509b9d5cb4ecf72
Sandeep Panuganti <sp21097@corp.ojas-it.com>
2022-12-28 14:04:20 +0530

    added changes

--- project-main/trackip/settings.py
+++ project-main/trackip/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'clogmtfz8xc3&+x1!=77duv@5iftjnr!^g)nug$-f46(t0+-47'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
